220d5bdbe0e184634e23f19c12152556ad474c6e
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-07-10 20:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function updateWindowSize() {
+            var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+            var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+            var sizeInfo = document.getElementById("sizeInfo");
+            sizeInfo.innerHTML = "<h1>Window Size: Width: " + width + "px, Height: " + height + "px</h1>";
+        }
+
+        window.addEventListener("resize", updateWindowSize);
+        window.addEventListener("DOMContentLoaded", updateWindowSize);
